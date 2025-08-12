ab55070509e2da1d37b30d9147da5a57e662e44c
Daniel Moura <95764920+Natsuzera@users.noreply.github.com>
2025-08-12 13:06:25 -0300

    Update archive_load.py

--- src/archive_load.py
+++ src/archive_load.py
@@ -4,7 +4,7 @@ import pandas as pd
 def load_data() -> pd.DataFrame:
     base_path = os.path.dirname(os.path.abspath(__file__))
     # Monta o caminho para o arquivo dentro da pasta "data"
-    file_path = os.path.normpath(os.path.join(base_path, '..', 'data', 'avaliacaodocencia.csv'))
+    file_path = os.path.normpath(os.path.join(base_path, '..', 'avaliacaodocencia.csv'))
     
     # Carrega o arquivo CSV
     df = pd.read_csv(file_path, sep=';')
